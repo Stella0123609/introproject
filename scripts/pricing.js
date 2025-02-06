e9f40e24a033074fe9057fd843b4ea6dfda27346
stella njau <smaries6016@gmail.com>
2025-02-06 17:03:14 +0300

    worked on pricing html

---
+++ scripts/pricing.js
@@ -0,0 +1,12 @@
+function func2(){
+    document.getElementById("pricing-monthly").classList.remove("hidden")
+    document.getElementById("pricing-yearly").classList.add("hidden")
+}
+function func1(){
+    document.getElementById("pricing-monthly").classList.add("hidden")
+    document.getElementById("pricing-yearly").classList.remove("hidden")
+}
+function func3(){
+    document.getElementById("pricing-monthly").classList.remove("hidden")
+    document.getElementById("pricing-yearly").classList.remove("hidden")
+}
