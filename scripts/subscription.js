a21bf51eafe6af5494d037d3aaab6569880735ed
stella njau <smaries6016@gmail.com>
2025-02-07 11:25:48 +0300

    added a bmi

--- scripts/subscription.js
+++ scripts/subscription.js
@@ -1,3 +1,26 @@
+
+function calculateBMI() {
+  let unit= document.getElementById("unit").value;
+  let weight = parseFloat(document.getElementById("weight").value);
+  let height = parseFloat(document.getElementById("height").value);
+            let bmi;
+
+   if (unit === "imperial") {
+    weight *= 0.453592; // Convert lbs to kg
+    height *= 2.54; // Convert inches to cm
+  }
+    height /= 100; // Convert cm to meters
+    bmi = weight / (height * height);
+
+    let status;
+    if (bmi < 18.5) status = "Underweight";
+     else if (bmi < 24.9) status = "Normal";
+     else if (bmi < 29.9) status = "Overweight";
+     else status = "Obese";
+
+ document.getElementById("result").innerText = `Your BMI: ${bmi.toFixed(2)} - ${status}`;
+        }
+    
 function func2(){
     document.getElementById("pricing-monthly").classList.remove("hidden")
     document.getElementById("pricing-yearly").classList.add("hidden")
